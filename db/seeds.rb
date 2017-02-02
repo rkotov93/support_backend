4e578e9ac31e626c5ff0c42ee971eb422255d562
Ruslan Kotov <rkotov93@gmail.com>
2017-02-03 00:12:34 +0200

    Add db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+FactoryGirl.create(:user, role: 'admin', email: 'admin@crossover.com', password: 'qweasd')
+
+2.times.each do
+  FactoryGirl.create(:user, role: 'support')
+end
+
+6.times.each do
+  FactoryGirl.create(:user)
+end
+
+customers = User.where(role: 'customer').first(2)
+
+8.times.each do
+  FactoryGirl.create(:ticket, author: customers.sample)
+end
